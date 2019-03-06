102456a036a775cf20a1ee84cb4b55a7b634668b
Anwesh43 <anweshthecool0@gmail.com>
2019-03-07 02:33:34 +0530

    adding Int and Float extension methods

--- circlepathrotview/src/main/java/com/anwesh/uiprojects/circlepathrotview/CirclePathRotView.kt
+++ circlepathrotview/src/main/java/com/anwesh/uiprojects/circlepathrotview/CirclePathRotView.kt
@@ -15,9 +15,13 @@ import android.content.Context
 
 val nodes : Int = 5
 val paths : Int = 2
-val scGap : Float = 0.05f
-val scDiv : Double = 0.51
+val scGap : Float = 0.1f
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#212121")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = dir * scGap
