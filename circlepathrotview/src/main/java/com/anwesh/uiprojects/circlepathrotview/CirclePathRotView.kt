2842471ba76dcc74f5265ed9daf88f334df53f1f
Anwesh43 <anweshthecool0@gmail.com>
2019-03-07 02:30:25 +0530

    adding libraries and defining constants

--- circlepathrotview/src/main/java/com/anwesh/uiprojects/circlepathrotview/CirclePathRotView.kt
+++ circlepathrotview/src/main/java/com/anwesh/uiprojects/circlepathrotview/CirclePathRotView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.circlepathrotview
  * Created by anweshmishra on 07/03/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val paths : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#212121")
